ab604d5d96e6671bee6db9313c09010935a5a1a2
garrettjwilke <garrett@system76.com>
2025-08-29 11:00:10 -0600

    fixed linux build

---
+++ external/qualetize_math_header.h
@@ -0,0 +1,9 @@
+#ifndef QUALETIZE_MATH_HEADER
+#define QUALETIZE_MATH_HEADER
+#define _GNU_SOURCE
+#include <math.h>
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif // M_PI
+#endif // QUALETIZE_MATH_HEADER
+
